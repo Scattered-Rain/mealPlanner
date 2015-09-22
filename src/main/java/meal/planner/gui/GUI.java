5f6cbdf09b93b512a85d418d171b1e323f6c2fc6
ggperseus <ggperseus@gmail.com>
2015-09-21 23:01:49 -0400

    Realized I was still working in Master, comitting changes and going to develop

--- src/main/java/meal/planner/gui/GUI.java
+++ src/main/java/meal/planner/gui/GUI.java
@@ -3,6 +3,8 @@ package meal.planner.gui;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import com.alee.laf.menu.WebMenu;
+import com.alee.laf.menu.WebMenuBar;
 import com.alee.laf.rootpane.WebFrame;
 
 public class GUI {
@@ -11,6 +13,11 @@ public class GUI {
 
 	/** Instantiates the GUI */
 	public GUI() {
+		initialize();
+
+	}
+
+	public void initialize() {
 		mainFrame = new WebFrame();
 
 		// Set to the size of the screen
@@ -21,5 +28,15 @@ public class GUI {
 		mainFrame.setDefaultCloseOperation(WebFrame.EXIT_ON_CLOSE);
 		mainFrame.setVisible(true);
 
+	}
+
+	public void setupMenu() {
+		WebMenuBar menuBar = new WebMenuBar();
+		mainFrame.setJMenuBar(menuBar);
+
+		WebMenu menu = new WebMenu("File");
+		menuBar.add(menu);
+
+		// TODO: populate menu
 	}
 }
