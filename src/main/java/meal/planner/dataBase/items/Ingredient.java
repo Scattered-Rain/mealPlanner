0873c4c34f86fbf4ccfd739be8047f5d08592f94
Scattered-Rain <Florian221@gmx.net>
2015-09-25 12:58:17 -0400

    Added asserions EVERYWHERE O.O

--- src/main/java/meal/planner/dataBase/items/Ingredient.java
+++ src/main/java/meal/planner/dataBase/items/Ingredient.java
@@ -16,6 +16,7 @@ public class Ingredient extends Item<IngAttribute>{
 	
 	/** Returns whether the Ingredient contains the given IngAttribute */
 	public boolean contains(IngAttribute att){
+		assert isOk();
 		for(IngAttribute a : subItems){
 			if(a==att){
 				return true;
@@ -29,16 +30,19 @@ public class Ingredient extends Item<IngAttribute>{
 				}
 			}
 		}
+		assert isOk();
 		return false;
 	}
 	
 	/** Returns the price of this ingredient */
 	public float getPrice(){
+		assert isOk();
 		return price;
 	}
 	
 	/** Sets the price of the current Ingredient */
 	public void setPrice(float price){
+		assert isOk();
 		this.price = price;
 	}
 
